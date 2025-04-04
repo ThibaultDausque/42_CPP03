6a28d5c751cdeee2a9549458379087a71b3c0d35
Thibault Dausque <tdausque@2A1.42angouleme.fr>
2025-04-04 21:26:22 +0200

    ex00

---
+++ ex00/ClapTrap.hpp
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ClapTrap.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tdausque <tdausque@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/04 21:14:44 by tdausque          #+#    #+#             */
+/*   Updated: 2025/04/04 21:23:05 by tdausque         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <string>
+
+class ClapTrap
+{
+	private:
+		std::string	_name;
+		int			_hit;
+		int			_energy;
+		int			_damage;
+
+	public:
+		ClapTrap(std::string name);
+		ClapTrap(const ClapTrap& obj);
+		ClapTrap&	operator=(const ClapTrap&);
+		~ClapTrap();
+		void		attack(const std::string& target);
+		void		takeDamage(unsigned int amount);
+		void		beRepaired(unsigned int amount);
+};
+
